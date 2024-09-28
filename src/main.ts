66e09b6c9ae61741f677b6e5395d2f75db6bec72
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-09-28 14:57:06 +0530

    replacing kafka server and port number with env varibale, replacing console log with logger, adding crude ovbservation

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,31 @@
+import observation from './ObservationConfig';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['verbose'] });
+  const log: Logger = new Logger('main');
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
     options: {
       client: {
-        brokers: ['localhost:9092'],
+        brokers: [process.env.KAFKA_SERVER],
       },
       // this is this service's group-id
       consumer: {
         groupId: 'hangout-notification-service',
       },
-      // producer: {
-      //   allowAutoTopicCreation: false,
-      //   createPartitioner: Partitioners.DefaultPartitioner,
-      // },
     },
   });
-  // start all microservices.. (here kafka)
+  // starting otel observation
+  observation.start();
+  log.log('started observation');
+  // starting kafka microservice
   await app.startAllMicroservices();
-  console.log('starting http server at port 5012');
+  log.log('started kafka consumer service');
   // start the app with http server
-  await app.listen(5012);
+  await app.listen(process.env.port);
+  log.log(`starting http server at port ${process.env.port}`);
 }
 bootstrap();
